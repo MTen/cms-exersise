85c143f3f5e060562d99580159f0f107a4615079
Mike Tener <michaeltener@gmail.com>
2014-04-13 23:34:41 -0400

    added git link and removed interests

--- app/models/musing.rb
+++ app/models/musing.rb
@@ -1,2 +1,6 @@
 class Musing < ActiveRecord::Base
+
+  def self.tile
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140401230110) do
+ActiveRecord::Schema.define(version: 20140414023505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20140401230110) do
     t.string "url"
     t.string "img"
     t.text   "description"
+    t.string "github"
   end
 
 end
